2c913acddf6e604a6a79fa68554de5b245c26d45
kent1 <kent1@arscenic.info>
2014-07-29 09:35:14 +0200

    On ajoute le bloc timeline dans les blocs z

---
+++ alacarte_skel_options.php
@@ -0,0 +1,7 @@
+<?php
+
+if (!isset($GLOBALS['z_blocs']))
+	$GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer','breadcrumb','timeline');
+else
+	$GLOBALS['z_blocs'][] = 'timeline';
+
