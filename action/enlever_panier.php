12624ef53afa2f10a77546de63935a8b481d7ae4
kent1 <kent1@arscenic.info>
2014-07-29 10:34:31 +0200

    Une action pour supprimer les éléments du panier

---
+++ action/enlever_panier.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Action de suppression d'un élément du panier
+ * 
+ * On n'utilise pas les quantités
+ */
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+/**
+ * Remplir un panier avec un objet quelconque
+ * @param unknown_type $arg
+ * @return unknown_type
+ */
+function action_enlever_panier_dist($arg=null) {
+	if (is_null($arg)){
+		$securiser_action = charger_fonction('securiser_action', 'inc');
+		$arg = $securiser_action();
+	}
+	
+	// On récupère les infos de l'argument
+	@list($objet, $id_objet, $md5) = explode('-', $arg);
+	$id_objet = intval($id_objet);
+	
+	// Il faut cherche le panier du visiteur en cours
+	include_spip('inc/session');
+	$id_panier = session_get('id_panier');
+	// S'il n'y a pas de panier, on le crée
+	if (!$id_panier){
+		return;
+	}
+	
+	// On ne fait que s'il y a bien un panier existant et un objet valable
+	if ($id_panier > 0 and $objet and $id_objet and $md5) {
+		sql_delete(
+			'spip_paniers_liens',
+			'id_panier='.intval($id_panier).' AND objet='.sql_quote($objet).' AND md5='.sql_quote($md5)
+		);
+		
+		// Mais dans tous les cas on met la date du panier à jour
+		sql_updateq(
+			'spip_paniers',
+			array('date'=>'NOW()'),
+			'id_panier = '.intval($id_panier)
+		);
+	}
+}
+
+?>
