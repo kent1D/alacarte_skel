4ca56db1de93c1b5756d22ded3abb5116c1c027b
kent1 <kent1@arscenic.info>
2014-07-29 09:35:51 +0200

    Internationalisation

---
+++ lang/alacarte_en.php
@@ -0,0 +1,28 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// L
+	'label_color' => 'Color:',
+	'label_size' => 'Size:',
+	'label_stock' => 'In stock:',
+
+	// P
+	'panier_un_item' => '<span class="number">@nb@</span> ITEM',
+	'panier_nb_items' => '<span class="number">@nb@</span> ITEMS',
+	
+	// S
+	'subtitle_timeline_1' => 'NO MIN/MAX CHAPTERS',
+	'subtitle_timeline_2' => 'SHUFFLE THE CHAPTERS FREELY',
+	'subtitle_timeline_3' => 'SHIPPING COSTS NOT INCLUDED',
+
+	// T
+	'title_timeline' => 'CREATE YOUR OWN PERFORMANCE NOW!',
+	'title_timeline_step_1' => 'SELECT YOUR FAVORITE CHAPTERS',
+	'title_timeline_step_2' => 'SET PERFORMANCE DATE(S)/LOCATION',
+	'title_timeline_step_3' => 'ORDER YOUR PERFORMANCE',
+);
+
+?>
