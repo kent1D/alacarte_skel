4650136b36739549266d56f853d05feff0ff77a4
kent1 <kent1@arscenic.info>
2014-07-29 17:07:20 +0200

    sécurité

--- alacarte_skel_options.php
+++ alacarte_skel_options.php
@@ -1,5 +1,7 @@
 <?php
 
+if (!defined("_ECRIRE_INC_VERSION")) return;	#securite
+
 if (!isset($GLOBALS['z_blocs']))
 	$GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer','breadcrumb','timeline');
 else
