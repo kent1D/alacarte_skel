92742446e08d8e8cb714c08c4490cf9168e50155
kent1 <kent1@arscenic.info>
2014-07-31 22:48:48 +0200

    Les chaînes de langue récentes

--- lang/alacarte_en.php
+++ lang/alacarte_en.php
@@ -4,6 +4,13 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+	// C
+	'contact_chapo' => 'Do you have any doubts, comments, questions, suggestions? Or do you want to keep up to date?',
+	'contact_label_email' => 'Your e-mail',
+	'contact_label_message' => 'Your message',
+	'contact_message_placeholder' => 'WRITE YOUR MESSAGE HERE',
+	'contact_title_form' => 'DO YOU WANT TO GET IN CONTACT WITH US?',
+
 	// E
 	'erreur_no_video_criteria' => 'No video corresponding',
 
@@ -13,6 +20,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_filters_all' => 'ALL',
 	'label_size' => 'Size:',
 	'label_stock' => 'In stock:',
+
+	// M
+	'message_intro_abomailman' => 'If you wish to be informed about all the activities happening in connection with the À la Cart performance webshop project, subscribe to our NEWSLETTER.',
 
 	// P
 	'panier_un_item' => '<span class="number">@nb@</span> ITEM',
@@ -24,6 +34,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'subtitle_timeline_3' => 'SHIPPING COSTS NOT INCLUDED',
 
 	// T
+	'title_abomailman' => 'KEEP UP TO DATE',
+	'title_contact_infos' => 'CONTACT INFORMATION',
+	'title_social_contact' => 'SOCIAL CONTACT',
 	'title_timeline' => 'CREATE YOUR OWN PERFORMANCE NOW!',
 	'title_timeline_step_1' => 'SELECT YOUR FAVORITE CHAPTERS',
 	'title_timeline_step_2' => 'SET PERFORMANCE DATE(S)/LOCATION',
---
+++ lang/contact_en.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/contact?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'champ_nom' => 'Your name',
+	'champ_sujet' => 'Your subject',
+
+);
+
+?>
