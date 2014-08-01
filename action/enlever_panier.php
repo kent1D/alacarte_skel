976f4281ddce00375b30aa7604b7cb2b14613204
kent1 <kent1@arscenic.info>
2014-08-01 02:13:53 +0200

    Lors de la suppression, modifier les rangs

--- action/enlever_panier.php
+++ action/enlever_panier.php
@@ -32,11 +32,19 @@ function action_enlever_panier_dist($arg=null) {
 	
 	// On ne fait que s'il y a bien un panier existant et un objet valable
 	if ($id_panier > 0 and $objet and $id_objet and $md5) {
+		$rang = sql_getfetsel('rang','spip_paniers_liens','id_panier='.intval($id_panier).' AND objet='.sql_quote($objet).' AND md5='.sql_quote($md5));
 		sql_delete(
 			'spip_paniers_liens',
 			'id_panier='.intval($id_panier).' AND objet='.sql_quote($objet).' AND md5='.sql_quote($md5)
 		);
 		
+		$rang_plus = sql_allfetsel('*','spip_paniers_liens','id_panier='.intval($id_panier).' AND objet='.sql_quote($objet).' AND rang > '.intval($rang));
+		foreach($rang_plus as $produit){
+			spip_log($produit,'test.'._LOG_ERREUR);
+			$nouveau_rang = intval($produit['rang'])-1;
+			spip_log($nouveau_rang,'test.'._LOG_ERREUR);
+			sql_updateq('spip_paniers_liens',array('rang'=>$nouveau_rang),'objet='.sql_quote($produit['objet']).' AND md5='.sql_quote($produit['md5']));
+		}
 		// Mais dans tous les cas on met la date du panier à jour
 		sql_updateq(
 			'spip_paniers',
