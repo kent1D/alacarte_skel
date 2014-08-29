51bacfb1f695aaaab14d145d089f0132eb95b4b3
kent1 <kent1@arscenic.info>
2014-08-29 13:42:36 +0200

    Améliorer le poid des vidéos (XL/L/M/S/XS)

--- alacarte_skel_fonctions.php
+++ alacarte_skel_fonctions.php
@@ -6,13 +6,13 @@ function alacarte_weight_video($duree=0){
 	if(intval($duree) < 60){
 		return 'XS';
 	}
-	else if(intval($duree) < 90){
+	else if(intval($duree) < 180){
 		return 'S';
 	}
-	else if(intval($duree) < 150){
+	else if(intval($duree) < 360){
 		return "M";
 	}
-	else if(intval($duree) < 300){
+	else if(intval($duree) < 540){
 		return "L";
 	}
 	else return "XL";
