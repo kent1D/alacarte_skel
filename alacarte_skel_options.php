86cc916987a7d9d5ef6c80682f0d684cefeacc61
kent1 <kent1@arscenic.info>
2014-09-04 19:19:54 +0200

    Ne pas envoyer de mail d'inscription

--- alacarte_skel_options.php
+++ alacarte_skel_options.php
@@ -2,8 +2,13 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;	#securite
 
+//define('_LOG_FILTRE_GRAVITE',8);
+
 if (!isset($GLOBALS['z_blocs']))
 	$GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer','breadcrumb','timeline');
 else
 	$GLOBALS['z_blocs'][] = 'timeline';
 
+function envoyer_inscription($desc, $nom, $mode, $id) {
+	return false;
+}
